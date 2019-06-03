0cb09e01cd37e41b8cffaed0bc6c930899bdb717
Aodhan Hayter <aodhan.hayter@gmail.com>
2019-06-03 16:43:10 -0600

    Add solutions

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+import TopicBrowser from './components/TopicBrowser/TopicBrowser';
+
+function App() {
+  return (
+    <TopicBrowser />
+  )
+}
+
+export default App;
