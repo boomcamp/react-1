5688a3e7675b17870e0d41061af2b197acf9ae74
AJAlilano23 <alan.alilano@boom.camp>
2019-06-17 20:38:51 +0800

    Fixed TopicBrowser.js

--- src/components/TopicBrowser/TopicBrowser.js
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import EvenAndOdd from './components/Topics/EvenAndOdd';
-import FilterObject from './components/Topics/FilterObject';
-import FilterString from './components/Topics/FilterString';
-import Palindrome from './components/Topics/Palindrome';
-import Sum from './components/Topics/Sum';
+import EvenAndOdd from '../Topics/EvenAndOdd';
+import FilterObject from '../Topics/FilterObject';
+import FilterString from '../Topics/FilterString';
+import Palindrome from '../Topics/Palindrome';
+import Sum from '../Topics/Sum';
 
 function TopicBrowser(){
   return (
