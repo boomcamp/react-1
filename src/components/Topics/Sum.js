5879b87f6ef56e1f9024bfdb1e4d96a805a9e421
AJAlilano23 <alan.alilano@boom.camp>
2019-06-17 22:15:36 +0800

    Step 7 Done

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Sum(){
+  const [userInput, setUserInput] = useState('');
+  const [sum, setSum] = useState([]);
+
+  function add(input){
+    const sum = input
+      .split(',')
+      .map(x => parseInt(x))
+      .reduce((accumulator, number) => {
+        accumulator += number;
+        return accumulator;
+      }, 0)
+
+    setSum(sum);
+  }
+
   return (
-    <p> Sum Component </p>
+    <div className="puzzleBox sumPB">
+      <h4> Sum </h4>
+      <input className="inputLine" onChange={e => setUserInput(e.target.value)}></input>
+      <button className="confirmationButton" onClick={() => add(userInput)}> Add </button>
+      <span className="resultsBox">Sum: {JSON.stringify(sum)}</span>
+    </div>
   )
 }
 
