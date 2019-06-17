bb767f6a36c4d78233a45e75bb140a4ac62bce99
judesama <judeagagad@gmail.com>
2019-06-17 09:48:50 +0800

    done

---
+++ src/components/Topics/Palindrome.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+export default function Palindrome() {
+
+  const [userInput , setUserInput] = useState('');
+  const [answer, setAnswer] = useState('Enter a word to check');
+
+  function checkString(word){
+    
+    const normalizedWord = word.toLowerCase();
+    const reversedWord = normalizedWord
+      .split('')
+      .reverse()
+      .join('');
+    setAnswer(normalizedWord === reversedWord);
+
+  }
+
+  
+
+  return (
+    <div className="puzzleBox palindromePB">
+      <h4> Palindrome </h4>
+      <input className="inputLine" onChange={(e) => setUserInput(e.target.value)}></input>
+      <button className="confirmationButton" onClick={()=> checkString(userInput)}> Check </button>
+      <span className="resultsBox">{JSON.stringify(answer)}</span>
+    </div>
+  )
+}
---
+++ src/components/Topics/Sum.js
@@ -0,0 +1,33 @@
+    
+import React, { useState } from 'react';
+
+export default function Sum() {
+  const [userInput, setUserInput] = useState('');
+  const [sum, setSum] = useState();
+
+  function getSum(input) {
+    const sum = input
+      .split(',')
+      .map(n => parseInt(n))
+      .reduce((acc, n) => {
+        acc += n;
+        return acc;
+      }, 0);
+
+    setSum(sum);
+  }
+
+  return (
+    <div className="puzzleBox sumPB">
+      <h4>Sum</h4>
+      <input
+        className="inputLine"
+        onChange={e => setUserInput(e.target.value)}
+      />
+      <button className="confirmationButton" onClick={() => getSum(userInput)}>
+        Add
+      </button>
+      <span className="resultsBox">{JSON.stringify(sum)}</span>
+    </div>
+  );
+}
