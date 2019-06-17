b46086d118ca64e0a82d6f1f24d2395f944d16f0
AJAlilano23 <alan.alilano@boom.camp>
2019-06-17 21:43:30 +0800

    Step 4 Done

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function FilterObject(){
+  const [unfilteredArray, setUnfilteredArray] = useState([
+    { name:  'John', title: 'Junior Developer', age: 20 },
+    { name: 'Bob', title: 'Development Manager', age: 32, yearsEmployed: 2 },
+    { name: 'Lisa', title: 'Senior Developer' },
+  ]);
+  const [filteredArray, setFilteredArray] = useState([]);
+  const [userInput, setUserInput] = useState('');
+
+  function filter(input){
+    const filtered = unfilteredArray.filter(a =>{
+      if (a[input]) {
+        return true;
+      }
+      return false;
+    })
+    setFilteredArray(filtered);
+    
+  }
+
   return (
-    <p> FilterObject Component </p>
+    <div className="puzzleBox filterObjectPB">
+      <h4>Filter Object</h4>
+      <span className="puzzleText">Original: {JSON.stringify(unfilteredArray, null, 10)}</span>
+      <input className="inputLine" onChange={e => setUserInput(e.target.value)}/>
+      <button className="confirmationButton" onClick={() => filter(userInput)}>Filter</button>
+      <span className="resultsBox filterObjectRB">Filtered: {JSON.stringify(filteredArray, null, 10)}</span>
+    </div>
   )
 }
 
