6ad0dc314f8fe4b559e6eab8890d49c0859e0bce
AJAlilano23 <alan.alilano@boom.camp>
2019-06-17 21:10:01 +0800

    Fixed EvenAndOdd and Step 3 Done

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function EvenAndOdd(){
+  const [evens, setEvens] = useState([]);
+  const [odds, setOdds] = useState([]);
+  const [userInput, setUserInput] = useState('');
+
+  function solve(input){
+    const results = {
+      evens: [],
+      odds: []
+    }
+    for(let x of input.split(',').map(arr => parseInt(arr))){
+      if(x % 2 === 0){
+        results.evens.push(x);
+      }else{
+        results.odds.push(x);
+      }
+    }
+
+    setEvens(results.evens);
+    setOdds(results.odds);
+  }
+
   return (
-    <p> EvenAndOdd Component </p>
+    <div className="puzzleBox evenAndOddPB">
+      <h4>Evens and Odds</h4>
+      <input className="inputLine" onChange={e => setUserInput(e.target.value)}/>
+      <button className="confirmationButton" onClick={() => solve(userInput)}>Split</button>
+      <span className="resultsBox">Evens: {JSON.stringify(evens)}</span>
+      <span className="resultsBox">Odds: {JSON.stringify(odds)}</span>
+    </div>
   )
 }
 
