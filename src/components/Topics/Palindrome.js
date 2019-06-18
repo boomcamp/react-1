c2331b0451b4d0741038f43693dd606cd8bb79bc
Sid <bercasio.sidney@gmail.com>
2019-06-18 10:04:59 +0800

    Done Step 6

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React , {useState}  from 'react';
 
 export default function Palindrome() {
+    const [input, setInput] = useState('');
+    const [result, setResult] = useState('Enter a word to check');
+    function check(inp){
+        if(inp === inp.split('').reverse().join('')){
+            setResult('true')
+        }
+        else{
+            setResult('false')
+        }
+    }
   return (
-    <p> Palindrome Component </p>
+    <div className="puzzleBox palindromePB">
+        <h4> Palindrome </h4>
+        <input onChange={e => setInput(e.target.value)}className="inputLine"></input>
+        <button onClick={() => check(input)} className="confirmationButton"> Check </button>
+        <span className="resultsBox">{result}</span>
+    </div>
   )
 }
