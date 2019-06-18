5c34d8f19675a01fc707452d10e3b4f12daca78f
Emelyn Soria <50132975+emelynsoria@users.noreply.github.com>
2019-06-18 09:33:55 +0800

    Update Sum.js

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,22 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Sum() {
     const [sum, setSum] = useState();
     const [userInput, setUserInput] = useState('');
 
     function outputSum(string) {
-            // const sumOfNums = 0;
-            // const num = string.split(',');
-
-            // for(let i=0; i < num.length; i++){
-            //     var num1 = parseInt.input[0];
-            //     var num2 = parseInt.input[length - 1];
-            //     sumOfNums = num1 + num2;
-            // }
-            // setSum(sumOfNums);
+            var num = 0;
 
             for(let val of string.split(',').map(n => parseInt(n))) {
-                var num = num + val;
+                num = num + val;
             }
             setSum(num);
     }
