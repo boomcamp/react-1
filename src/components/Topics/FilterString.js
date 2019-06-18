9f60b73f5e3deb54742d8d28bb4a3c5f5e477ca2
Noel Layante <noel.layante@boom.camp>
2019-06-18 14:50:36 +0800

    Done Step 5

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function FilterString() {
+  const [names, setNames] = useState([
+    'Spider-Man',
+    'Iron Man',
+    'Thor',
+    'Captain America',
+    'Hulk',
+    'Black Widow'
+  ]);
+
+  const [userInput, setUserInput] = useState('');
+  const [filteredNames, setFilteredNames] = useState([]);
+
+  function filter(userInput){
+    setFilteredNames(
+      names.filter(names => names.includes(userInput))
+    );
+  }
+
   return (
-    <p>FilterString</p>
+    <div className="puzzleBox filterStringPB">
+      <h4>Filter String</h4>
+      <span className="puzzleText">
+        Names: {JSON.stringify(names)}
+      </span>
+      <input className="inputLine" onChange={event => setUserInput(event.target.value)}></input>
+      <button className="confirmationButton" onClick={() => filter(userInput)}>Filter</button>
+      <span className="resultsBox filterStringRB">
+        Filtered Names: {JSON.stringify(filteredNames)};
+      </span>
+    </div>
   )
 }
