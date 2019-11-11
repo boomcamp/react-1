b08f903703426f0b4d17be3a81ce8965cb32b356
ginoAquino7 <gino.aquino@boom.camp>
2019-11-11 17:42:20 +0800

    submission

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,23 +1,26 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 
 export default function EvenAndOdd(){
+    
   const [add, setAdd ] = useState([]);
   const [userInput, setUserInput] = useState('');
-  function getSum(input) {
+
+  function getSum(input){
     const results = {
       add: []
     } 
     var  sum = input.toString().split('').map(Number).reduce(function (a, b) {
             return a + b;
         }, 0);   
+
     results.add.push(sum)
     setAdd(results.add)
   }
   return ( 
     <div className="puzzleBox sumPB">
     <h4> Sum </h4>
-    <input className="inputLine"  onChange={c=> setUserInput(c.target.value)} type ="number"></input>
+    <input className="inputLine" onChange={e=> setUserInput(e.target.value)} type ="number"></input>
     <button className="confirmationButton" onClick={() => getSum(userInput)}> Add </button>
     <span className="resultsBox">{add}</span>
   </div>
