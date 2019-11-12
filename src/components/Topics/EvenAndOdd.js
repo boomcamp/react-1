29955987f250c859abbf26895cd39b25c1e48166
rapraquion <rapraquion@gmail.com>
2019-11-12 02:28:30 -0500

    solved evenandodd component

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+export default function EvenAndOdd() {
+  const [evens, setEvens] = useState([]);
+  const [odds, setOdds] = useState([]);
+  const [userInput, setUserInput] = useState("");
+
+  const solve = input => {
+    const results = {
+      evens: [],
+      odds: []
+    };
+    for (let val of input.split(",").map(v => parseInt(v))) {
+      if (val % 2 === 0) {
+        results.evens.push(val);
+      } else {
+        results.odds.push(val);
+      }
+    }
+    setEvens(results.evens);
+    setOdds(results.odds);
+  };
+
+  return (
+    <div className="puzzleBox evenAndOddPB">
+      <h4>Evens and Odds</h4>
+      <input
+        className="inputLine"
+        onChange={e => setUserInput(e.target.value)}
+      ></input>
+      <button onClick={() => solve(userInput)} className="confirmationButton">
+        Split
+      </button>
+      <span className="resultsBox">Evens: [{evens}]</span>
+      <span className="resultsBox">Odds: [{odds}]</span>
+    </div>
+  );
+}
---
+++ src/components/Topics/Palindrome.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Palindrome() {
+  return <p>Palindrome Component</p>;
+}
