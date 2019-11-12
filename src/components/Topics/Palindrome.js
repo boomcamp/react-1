e40672394b28a6280d74e3a34ffb7890971f316e
rapraquion <rapraquion@gmail.com>
2019-11-12 03:36:58 -0500

    solved palindrome component

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -2,6 +2,16 @@ import React, { useState } from "react";
 
 export default function Palindrome() {
     const [userInput, setUserInput] = useState("");
+    const [result, setResult] = useState("");
+
+    function check(input) {
+        for (let i = 0; i < input.length / 2; i++) {
+            if (input[i] !== input[input.length - 1 - i])
+                return setResult("false");
+        }
+        return setResult("true")
+    }
+
     return (
         <div className="puzzleBox palindromePB">
             <h4>Palindrome</h4>
@@ -9,8 +19,8 @@ export default function Palindrome() {
                 className="inputLine"
                 onChange={e => setUserInput(e.target.value)}
             ></input>
-            <button className="confirmationButton">Check</button>
-            <span className="resultsBox">Enter a word to check</span>
+            <button onClick={() => check(userInput)} className="confirmationButton">Check</button>
+            <span className="resultsBox">{result}</span>
         </div>
     );
 }
